346b6a4a0bf87be1c0aeeaa4d9f0dfefb11d8fec
tylerjamesdinkle <mr.wattscompany@gmail.com>
2021-10-03 15:52:00 -0500

    updating hello world

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -2,7 +2,8 @@ package tests;
 
 public class HelloWorldTest {
     public static void main(String[] args) {
-
         System.out.println("Hello World");
+
+        System.out.println("One more hello");
     }
 }
